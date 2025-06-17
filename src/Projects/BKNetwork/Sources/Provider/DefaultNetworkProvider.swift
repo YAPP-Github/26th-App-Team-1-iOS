474a7c45242d8349d0314096c5a06c78f4e597f9
Jiyong Jung <hickerd@sch.ac.kr>
2025-06-18 00:08:59 +0900

    [BOOK-40] feat: Implement NetworkProvider

--- src/Projects/BKNetwork/Sources/Provider/DefaultNetworkProvider.swift
+++ src/Projects/BKNetwork/Sources/Provider/DefaultNetworkProvider.swift
@@ -1,14 +1,39 @@
 // Copyright © 2025 Booket. All rights reserved
 
-import Foundation
 import BKData
+import Foundation
+
+public protocol NetworkRequestable {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse)
+}
 
 public struct DefaultNetworkProvider: NetworkProvider {
+    private let requestor: NetworkRequestable
+    
+    init(requestor: NetworkRequestable) {
+        self.requestor = requestor
+    }
+    
     @discardableResult
     public func request<T: Decodable>(
         target: RequestTarget,
         type: T.Type
     ) async throws -> T {
-
+        let request = try target.makeURLRequest()
+        let (data, response) = try await requestor.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+        
+        guard let responseDTO = try? JSONDecoder().decode(T.self, from: data) else {
+            switch httpResponse.statusCode {
+            case 401: throw NetworkError.unauthorized
+            case 500...599: throw NetworkError.internalServerError
+            default: throw NetworkError.invalidResponse
+            }
+        }
+        
+        return responseDTO
     }
 }
