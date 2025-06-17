44c53031d68421dff5505f3e340d4eb905898b8d
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-06-17 16:55:09 +0900

    [BOOK-41] chore: delete path debug

--- src/Tuist/ProjectDescriptionHelpers/SwiftLintScript.swift
+++ src/Tuist/ProjectDescriptionHelpers/SwiftLintScript.swift
@@ -10,11 +10,8 @@ public let swiftLintScript: TargetScript = .pre(
     export PATH="/opt/homebrew/bin:$PATH"
     
     SWIFTLINT_PATH=$(which swiftlint)
-    echo "❗️SwiftLint Path: $SWIFTLINT_PATH"
     SWIFTLINT_CONFIG_PATH="${SRCROOT%/*/*}/.swiftlint.yml"
-    echo "❗️SRCROOT Path: ${SWIFTLINT_CONFIG_PATH}"
         
-    # SwiftLint 실행
     if [ -n "$SWIFTLINT_PATH" ]; then
         swiftlint --config "$SWIFTLINT_CONFIG_PATH"
     else
