f272559977231afa639c37297ac3f41869b3baa1
Jiyong Jung <hickerd@sch.ac.kr>
2025-06-17 16:52:23 +0900

    [BOOK-41] fix: Resolve swiftlint error

--- src/Tuist/ProjectDescriptionHelpers/SwiftLintScript.swift
+++ src/Tuist/ProjectDescriptionHelpers/SwiftLintScript.swift
@@ -7,10 +7,18 @@ let swiftLintConfigPath = "\(projectDir)/../../.swiftlint.yml"
 
 public let swiftLintScript: TargetScript = .pre(
     script: """
-    if which swiftlint > /dev/null; then
-      swiftlint --config "\(swiftLintConfigPath)"
+    export PATH="/opt/homebrew/bin:$PATH"
+    
+    SWIFTLINT_PATH=$(which swiftlint)
+    echo "❗️SwiftLint Path: $SWIFTLINT_PATH"
+    SWIFTLINT_CONFIG_PATH="${SRCROOT%/*/*}/.swiftlint.yml"
+    echo "❗️SRCROOT Path: ${SWIFTLINT_CONFIG_PATH}"
+        
+    # SwiftLint 실행
+    if [ -n "$SWIFTLINT_PATH" ]; then
+        swiftlint --config "$SWIFTLINT_CONFIG_PATH"
     else
-      echo "warning: SwiftLint not installed"
+        echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
     fi
     """,
     name: "SwiftLint",
