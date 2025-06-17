56c4f3fbb609134274d329f9aeb1103245b72ac9
Jiyong Jung <hickerd@sch.ac.kr>
2025-06-18 00:02:12 +0900

    [BOOK-40] feat: Add network errors

---
+++ src/Projects/BKNetwork/Sources/Error/NetworkError.swift
@@ -0,0 +1,9 @@
+// Copyright © 2025 Booket. All rights reserved
+
+public enum NetworkError: Error {
+    case invalidURL
+    case invalidResponse
+    case unauthorized
+    case internalServerError
+    case timeout
+}
