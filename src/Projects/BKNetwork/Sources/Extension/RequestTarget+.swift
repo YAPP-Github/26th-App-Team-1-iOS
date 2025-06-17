1a30f7f6a37c777d2861bd301e4f44240611fd93
Jiyong Jung <hickerd@sch.ac.kr>
2025-06-18 00:05:48 +0900

    [BOOK-40] feat: Add extensions for implement networking features

--- src/Projects/BKNetwork/Sources/Extension/RequestTarget+.swift
+++ src/Projects/BKNetwork/Sources/Extension/RequestTarget+.swift
@@ -1,8 +1,22 @@
 // Copyright © 2025 Booket. All rights reserved
 
-import Foundation
 import BKData
+import Foundation
 
 extension RequestTarget {
+    var query: [String: Any] {
+        return [:]
+    }
     
+    func makeURLRequest(accessToken: String = "") throws -> URLRequest {
+        var request: URLRequest = try URLRequest(baseURL + path, query: query)
+        
+        request.makeURLHeaders(headers)
+        request.addAuthorization(accessToken)
+        request.httpMethod = method.rawValue
+        request.cachePolicy = .reloadIgnoringLocalCacheData
+        
+        if let body { request.setBody(body) }
+        return request
+    }
 }
