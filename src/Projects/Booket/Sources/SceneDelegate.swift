c9a96de05da2955c30298384564cc333b5e37876
Jiyong Jung <hickerd@sch.ac.kr>
2025-06-26 15:57:04 +0900

    [BOOK-69] feat: Add AppDelegate, SceneDelegate

---
+++ src/Projects/Booket/Sources/SceneDelegate.swift
@@ -0,0 +1,17 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+    }
+}
